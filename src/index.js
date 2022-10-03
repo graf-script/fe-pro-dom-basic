b31b6225b4d84286db52df07e31d2d069ab14251
Kirill Makhnovskiy <script.makhnovskiy@gmail.com>
2022-10-03 23:01:56 +0300

    ad hw

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+    let elem = document.getElementsByTagName('li');
+    for(let i = 0; i < elem.length; i++){
+        if ( i % 2 === 0 ){
+            elem[i].style.backgroundColor = 'red';
+        }
+    }
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+    let el = document.querySelector('li');
+    
+    
+    while(el = el.nextElementSibling){
+    
+        if(el.matches('.likedItem')){
+            el.style.backgroundColor = 'blue';
+        }
+    }
+};
